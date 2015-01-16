e639d1d085009eb3b176364faebbc18f205f2d62
abelsonlive <brianabelson@gmail.com>
2015-01-15 21:41:33 -0500

    fixtest

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ class TorpedoTests(unittest.TestCase):
 	def test(self):
 		ip1 = torpedo.get_ip()
 		torpedo.refresh_ip()
-		ip2 = torpedo.refresh_ip()
+		ip2 = torpedo.get_ip()
 		assert(ip1 != ip2)
